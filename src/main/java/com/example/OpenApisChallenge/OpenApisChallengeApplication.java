0b37f6ac5a5216c3bf49555632f9b5bec5c509e9
nahidswe <nahidswe@gmail.com>
2023-12-10 09:20:55 +0600

    backend initial setup added.

---
+++ src/main/java/com/example/OpenApisChallenge/OpenApisChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenApisChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApisChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApisChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OpenApisChallenge/OpenApisChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenApisChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApisChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
